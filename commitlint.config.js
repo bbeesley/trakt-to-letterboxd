3b360447b365e2fd36a36a9ab8c709bdca492e7a
Bill Beesley <bill.beesley@dazn.com>
2021-04-12 10:24:04 +0100

    ci(commitlint): allow large commit msg body

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,9 @@
 const { types } = require('conventional-commit-types');
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: { 'type-enum': [2, 'always', Object.keys(types)] },
+  rules: {
+    'type-enum': [2, 'always', Object.keys(types)],
+    'body-max-line-length': [0, 'always', 100],
+  },
 };
